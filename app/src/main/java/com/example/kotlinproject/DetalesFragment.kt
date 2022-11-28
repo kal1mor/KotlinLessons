844b3b02f8042a316e875b4cccf810038bd0fb46
Julia <yulya_kovalchuk_95@mail.ru>
2022-11-28 22:00:07 +0300

    recycle view lesson

--- app/src/main/java/com/example/kotlinproject/DetalesFragment.kt
+++ app/src/main/java/com/example/kotlinproject/DetalesFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 
 
 class DetalesFragment : Fragment() {
@@ -19,5 +21,21 @@ class DetalesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val detailsName = view.findViewById<TextView>(R.id.detalesName)
+        val detailsDate = view.findViewById<TextView>(R.id.detalesDate)
+        val detailsImage = view.findViewById<ImageView>(R.id.detalesImage)
+
+        val bundle = arguments
+
+        bundle?.let { safeBundle ->
+            val name = safeBundle.getString("name")
+            val date = safeBundle.getString("date")
+            val image = safeBundle.getInt("imageVIew")
+
+            detailsName.text = name
+            detailsDate.text = date
+            detailsImage.setBackgroundResource(image)
+        }
     }
 }
